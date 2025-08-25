9c8d2a916f09a3dfe6712d97747bd37b7aa4aeff
Ruan Neves <154479887+NevesRuan@users.noreply.github.com>
2025-08-25 20:28:27 -0300

    Add header and docstring to cadeira_preto.py

--- cadeira_preto.py
+++ cadeira_preto.py
@@ -1,1 +1,16 @@
+# Cabeçalho do arquivo:
+# é a parte inicial de um script Python, composta geralmente por comentários 
+# e informações adicionais como autor, data, licença, propósito do código ou 
+# até instruções especiais Ele não faz parte da execução do código em si, mas
+# serve para organização, padronização e metadados que ajudam outros
+# desenvolvedores ou  o sistema operacional a entender como rodar o arquivo.
 
+""" 
+Docstring:
+é uma string de documentação colocada logo após a definição de módulos, funções, 
+classes ou métodos, escrita entre aspas triplas (`""" ... """`). Diferente do 
+cabeçalho, ela é interpretada pelo Python e pode ser acessada em tempo de 
+execução (`.__doc__` ou `help()`). Sua função é explicar de forma estruturada 
+o que o código faz, quais parâmetros recebe, o que retorna e outras instruções, 
+sendo essencial para documentação oficial e geração automática de guias do programa.
+"""
