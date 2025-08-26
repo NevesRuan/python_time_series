beb5cd77549a3dbe12a216abf73d8d843564dc2f
Fabiano Dicheti <117326271+FabianoDicheti@users.noreply.github.com>
2025-08-26 14:22:39 -0300

    Update cadeira_preto.py
    boa...

--- cadeira_preto.py
+++ cadeira_preto.py
@@ -1,3 +1,11 @@
+# ----------------------------------------------------------------------------
+# Script: cadeira_preto.py
+# Created By: Ruan Neves
+# Created Date: 2025-08-25
+# Version: 1.0
+# ---------------------------------------------------------------------------
+
+
 # Cabeçalho do arquivo:
 # é a parte inicial de um script Python, composta geralmente por comentários 
 # e informações adicionais como autor, data, licença, propósito do código ou 
@@ -5,12 +13,6 @@
 # serve para organização, padronização e metadados que ajudam outros
 # desenvolvedores ou  o sistema operacional a entender como rodar o arquivo.
 
-# ----------------------------------------------------------------------------
-# Script: cadeira_preto.py
-# Created By: Ruan Neves
-# Created Date: 2025-08-25
-# Version: 1.0
-# ---------------------------------------------------------------------------
 
 """ 
 Docstring:
@@ -23,3 +25,37 @@ sendo essencial para documentação oficial e geração automática de guias do 
 """
 
 
+# parabéns... muito boa resposta
+
+
+######################################################################
+
+# implemente as funções abaixo e coloque as docstrings
+
+def maximo(nums):
+    """oque faz
+    oque recebe
+    oque retorna"""
+    # TODO: percorra a lista guardando o maior atual
+    ...
+def e_par(n: int) -> bool:
+    """ ... """
+    # TODO: retorne se n é par
+    ...
+def fatorial(n: int) -> int:
+    """   ...  """
+    # TODO: implemente de forma iterativa (sem recursão)
+    ...
+import re
+def limpa_texto(s: str) -> str:
+    """..."""
+    # TODO: converta s para minúsculo e remova pontuações como ,.;:!?'"()-_
+    ...
+def conta_vogais(s: str) -> int:
+    """....."""
+    # TODO: conte as vogais simples
+    ...
+def palindromo(s: str) -> bool:
+    """..."""
+    # TODO: normalizar (minúsculo, remover não alfanumérico) e comparar com o reverso
+    ...
